55b0d3ba47999f571f3e3c5fb65ed99051621282
Godspower Similoluwa <131388888+gpsimi@users.noreply.github.com>
2023-10-09 18:20:19 +0100

    Initial commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {headerLogo} from "../assets/images";
+import {hamburger} from "../assets/icons";
+
+export default function  Nav()  {
+  return (
+    <header className="padding-x py-8 absolute z-10 w-full">
+        <nav className="flex justify-between items-center max-container">
+            <a href="/">
+                <img 
+                 src={headerLogo} 
+                 alt="Logo"
+                 width={130} 
+                 height={29}
+                />
+            </a>
+        </nav>
+    </header>
+  )
+}
+
