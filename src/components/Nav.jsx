c263a1ce816303ecf10cc6a404dcc0447478ef0a
Godspower Similoluwa <131388888+gpsimi@users.noreply.github.com>
2023-10-09 22:40:10 +0100

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {headerLogo} from "../assets/images";
 import {hamburger} from "../assets/icons";
+import { navLinks } from '../constants';
 
 export default function  Nav()  {
   return (
@@ -15,7 +16,16 @@ export default function  Nav()  {
                 />
             </a>
             <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
-
+              {navLinks.map((item) => (
+                <li key={item.label}>
+                  <a 
+                    href={item.href}
+                    className="font-montserrat leading-normal text-lg text-slate-gray"
+                  >
+                    {item.label}
+                  </a>
+                </li>
+              ))}
             </ul>
         </nav>
     </header>
