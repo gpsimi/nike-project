c0b6bfecf51c738545e84bdea88b6368785dab3a
Godspower Similoluwa <131388888+gpsimi@users.noreply.github.com>
2023-10-12 20:26:41 +0100

    Update SuperQuality.jsx

--- src/assets/sections/SuperQuality.jsx
+++ src/assets/sections/SuperQuality.jsx
@@ -1,9 +1,16 @@
+import { Button } from "../../components/Button"
+
 const SuperQuality = () => {
   return (
     <section 
       id="about-us"
+      className="flex justify-btween items-center max-lg:flex-col gap-10 w-full max-container"
     >
-      SuperQuality
+      <div className="flex flex-1 flex-col">
+        <p className="text-xl font-montserrat text-coral">
+          Our Summer collections
+        </p>
+      </div>
     </section>
   )
 }
